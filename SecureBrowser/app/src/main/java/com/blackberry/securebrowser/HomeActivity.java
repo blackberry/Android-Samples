0935e8c4e2ac0e4600530e9bd2daa59cf8faa841
narulasanam <narulasanam@gmail.com>
2015-12-11 16:10:28 -0500

    updated copyright statement

--- SecureBrowser/app/src/main/java/com/blackberry/securebrowser/HomeActivity.java
+++ SecureBrowser/app/src/main/java/com/blackberry/securebrowser/HomeActivity.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014-2015 BlackBerry Limited.
+/* Copyright (c) 2011-2015 BlackBerry Limited.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,6 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.blackberry.securebrowser;
 
 import android.content.Context;
